5f5574e13846234672e428d2b5bc9a0cc6a07a46
joshdevkit <jshpch1996@gmail.com>
2024-05-22 09:04:17 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
 
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       'process.env': env
     },
     plugins: [react()],
+    base: "/react-chat-app.io/",
     optimizeDeps: {
       include: ['react-helmet-async']
     }
