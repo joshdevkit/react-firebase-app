baa3510aa671bd8fbf61729e9910fedbac4f0f01
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2024-05-25 21:15:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
-
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       'process.env': env
     },
     plugins: [react()],
-    base: /react-firebase-app.io/
+    base: '/react-firebase-app.io/', // Specify the correct base URL
     server: {
       port: 3000,
     },
